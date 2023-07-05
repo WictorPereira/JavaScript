36742d654ebdf146c3540d486e640ed9330fa49b
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2023-07-05 17:47:05 -0300

    condições em JS

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 90
+
+console.log( `A velocidade do seu carro e ${vel}, por Km/h `)
+if (vel > 60) { //condição simples
+    console.log('Voce ultrapassou a velocidade permitida. Multado!')
+}
+console.log ('Dirija sempre usando cinto de segurança')
