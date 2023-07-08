3dae3fe821c6e4b6b65f65dd61e3a209a9929534
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2023-07-07 21:35:55 -0300

    laço de repetição while

---
+++ aula13/node_29765093b87e5.tmp.js
@@ -0,0 +1,22 @@
+var c = 1
+do{
+    console.log(`passo: ${c}`)
+}while (c<=10)
+
+/*
+var c = 1
+while (c <=6 ) {
+    console.log(`passo: ${c}`)
+    c++
+}
+*/ 
+
+
+
+/*console.log('boa noite')
+console.log('boa noite')
+console.log('boa noite')
+console.log('boa noite')
+console.log('boa noite')
+console.log('boa noite')
+*/
